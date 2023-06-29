b8d69308dcc0bfc1349d31cd4f6f44997a8b6c59
Bar Tavor <mtavor556@gmail.com>
2023-06-29 17:50:49 +0300

    improvements

--- main.py
+++ main.py
@@ -4,52 +4,177 @@ import sys
 import subprocess
 from urllib.parse import unquote
 import quart
-from quart import request
+from quart import request, Response
 from quart_cors import cors
 import re
+import requests
+import ast
+import sounddevice as sd
+import numpy as np
+from google.cloud import speech
+import pyttsx3
+import traceback
+from radon.complexity import cc_visit
+from radon.metrics import h_visit
+from pylint import epylint as lint
 
 app = quart.Quart(__name__)
 app = cors(app, allow_origin="https://chat.openai.com")
+memory = {}
+aliases = {}
 
 
-def remove_ansi_escape_sequences(text):
-    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-    return ansi_escape.sub('', text)
-
+# todo: add plan JSON.
+# todo: add instruction JSON. maybe divide over all endpoints
+# todo: add init data to memory
+# todo: refactor after code modification
+# todo: analyze folder
+# todo: add auth
+# todo: automate plugin improvement
+# todo: add plugin template
 
 @app.post("/initialize")
 async def initialize_plugin():
     request_data = await quart.request.get_json(force=True)
     env_name = request_data.get("env_name", "debuggerGPT")
+    memory['environ'] = env_name
+    memory['w_dir'] = os.getcwd()
     try:
         # Check if the specified conda environment exists
         envs = subprocess.check_output(['conda', 'env', 'list']).decode('utf-8')
         if env_name not in envs:
             # Create the specified conda environment
             subprocess.run(['conda', 'create', '-n', env_name, 'python=3.10'], check=True)
-
-        instructions = {
-            "message": "Initialization successful. The conda environment '" + env_name + "' is ready to use.",
-            "instructions": "As a Python Code Debugging Plugin, I can help you debug Python code files,"
-                            " manage Python environments, and manipulate files on your computer."
-                            " Here are the main capabilities and how to use them: \n\n1) "
-                            "Initialization: Before using the plugin, initialize it by running the 'initialize_plugin' function. "
-                            "This function sets up a conda environment for running scripts. The default environment is 'debuggerGPT', "
-                            "but you can specify a different one during initialization or when running a script. \n\n2) "
-                            "Execute Shell Commands: You can ask me to 'execute a command' and provide the command as a string. "
-                            "If you want to run the command in a specific environment, include the 'env_name' in your request. "
-                            "\n\n3) Run Python Scripts: You can ask me to 'run a Python script in a specific environment' "
-                            "and provide the path to the script and the name of the environment. Before running a script for the first time, "
-                            "I will check the code for non-native dependencies and ensure they are installed in the chosen environment. \n\n4) "
-                            "Modify Files: You can use the 'modify a file' command to update the local file of the code. "
-                            "\n\n5) Install Python Packages: You can ask me to 'install Python packages' and provide a list of packages to install in a specific environment. "
-                            "\n\n6) Debug Code: To debug a code, run the code, examine the output and error messages, "
-                            "decide how to fix the code, and then use the 'modify a file' command to update the code. "
-                            "Repeat this process until the code runs without errors."
-        }
+        with open('ai_instructions/init.txt', 'r') as file:
+            instructions = {
+                "message": "Initialization successful. The conda environment '" + env_name + "' is ready to use.",
+                "instructions": file.read()
+            }
         return quart.Response(response=json.dumps(instructions), status=200)
     except subprocess.CalledProcessError as e:
         return quart.Response(response=f'Error during initialization: {str(e)}', status=400)
+
+
+@app.post("/speak")
+async def speak():
+    request_data = await quart.request.get_json(force=True)
+    text = request_data.get("text", "")
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    return quart.Response(response='OK', status=200)
+
+
+@app.route("/memory", methods=["POST"])
+async def remember():
+    request_data = await request.get_json(force=True)
+    key = request_data.get("key", "")
+    value = request_data.get("value", "")
+    alias = request_data.get("alias", False)
+    if alias:
+        aliases[key] = value
+    else:
+        memory[key] = value
+    return Response(response='OK', status=200)
+
+
+@app.route("/memory/<key>", methods=["GET"])
+async def recall(key):
+    key = unquote(key)
+    if key in memory:
+        return Response(response=json.dumps({"value": memory[key]}), status=200)
+    elif key in aliases:
+        return Response(response=json.dumps({"value": aliases[key]}), status=200)
+    else:
+        return Response(response='Key not found', status=400)
+
+
+@app.post("/listen")
+async def listen():
+    # Record audio
+    duration = 10  # seconds
+    fs = 44100  # Sample rate
+    channels = 1  # Number of channels
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
+    sd.wait()  # Wait for the recording to finish
+
+    # Transcribe audio to text
+    client = speech.SpeechClient()
+    audio = speech.RecognitionAudio(content=np.array(myrecording).tobytes())
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=fs,
+        language_code="en-US",
+    )
+    response = client.recognize(config=config, audio=audio)
+
+    # Extract the transcription and return it
+    for result in response.results:
+        return {"transcription": result.alternatives[0].transcript}
+
+
+@app.get("/analyze_code")
+async def analyze_code():
+    try:
+        code_path = quart.request.args.get("code_path", "")
+        scope_level = int(quart.request.args.get("scope_level", "2"))
+        with open(code_path, 'r') as f:
+            code = f.read()
+        tree = ast.parse(code)
+        functions = [node.name for node in ast.walk(tree) if isinstance(node, ast.FunctionDef)]
+        dependencies = [alias.name for node in ast.walk(tree) if isinstance(node, ast.Import) for alias in node.names]
+        code_analysis = {
+            'length': len(code.split('\n')),
+            'functions': functions,
+            'code': code,
+            'dep': dependencies
+        }
+        if scope_level > 1:
+            classes = [node.name for node in ast.walk(tree) if isinstance(node, ast.ClassDef)]
+            comments = [node.s for node in ast.walk(tree) if isinstance(node, ast.Str)]
+            code_analysis.update({
+                'classes': classes,
+                'comments': comments,
+            })
+        if scope_level > 2:
+            (pylint_stdout, pylint_stderr) = lint.py_run(code_path, return_std=True)
+            pylint_output = pylint_stdout.getvalue()
+            cyclomatic_complexity = cc_visit(code)
+            halstead_metrics = h_visit(code)
+            code_analysis.update({
+                'pylint_output': pylint_output,
+                'cyclomatic_complexity': cyclomatic_complexity,
+                'halstead_metrics': halstead_metrics,
+            })
+        with open('ai_instructions/code_analysis.txt', 'r') as file:
+            instructions = file.read()
+        return quart.Response(response=json.dumps({"analysis": code_analysis, "instructions": instructions}),
+                              status=200)
+    except Exception as e:
+        tb_str = traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__)
+        print("".join(tb_str))  # Print the traceback
+        return quart.Response(response=json.dumps({"error": str(e)}), status=400)
+
+
+@app.post("/download_file")
+async def download_file():
+    request_data = await quart.request.get_json(force=True)
+    url = request_data.get("url", "")
+    local_path = request_data.get("local_path", "")
+    response = requests.get(url)
+    with open(local_path, 'wb') as f:
+        f.write(response.content)
+    return quart.Response(response='File downloaded successfully', status=200)
+
+
+@app.get("/list_files")
+async def list_files():
+    request_data = await quart.request.get_json(force=True)
+    folder_path = request_data.get("folder_path", "")
+    file_dict = {}
+    for root, dirs, files in os.walk(folder_path):
+        file_dict[root] = files
+    return quart.Response(response=json.dumps(file_dict), status=200)
 
 
 @app.post("/execute")
@@ -66,33 +191,19 @@ async def execute_command():
         return quart.Response(response=json.dumps({"error": str(e), "output": e.output}), status=400)
 
 
-@app.post("/install_packages")
-async def install_packages():
-    request_data = await quart.request.get_json(force=True)
-    env_name = request_data.get("env_name", "debuggerGPT")  # Default to 'debuggerGPT' if not specified
-    packages = request_data.get("packages", [])
-    for package in packages:
-        command = f"conda install -n {env_name} {package} -y"
-        try:
-            subprocess.run(command, shell=True, check=True)
-        except subprocess.CalledProcessError as e:
-            return quart.Response(response=json.dumps({"error": str(e)}), status=400)
-    return quart.Response(response='Packages installed successfully', status=200)
-
-
 @app.post("/run_script")
 async def run_script():
     request_data = await quart.request.get_json(force=True)
     env_name = request_data.get("env_name", "debuggerGPT")  # Default to 'debuggerGPT' if not specified
     script_path = request_data.get("script_path", "")
-    command = f"conda run -n {env_name} python {script_path}"
+    args = request_data.get("args", [])  # Get the arguments from the request data
+    args_str = " ".join(args)  # Convert the list of arguments into a string
+    command = f"conda run -n {env_name} python {script_path} {args_str}"  # Include the arguments in the command
     try:
         result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
         processed_output = remove_ansi_escape_sequences(result.stdout)
-        # print(result.stderr)
         return quart.Response(response=json.dumps({"output": processed_output, "error": result.stderr}), status=200)
     except subprocess.CalledProcessError as e:
-        # print(e.output)
         return quart.Response(response=json.dumps({"error": e.stderr, "output": e.output, "stderr": str(e)}),
                               status=400)
 
@@ -109,10 +220,14 @@ async def get_file(filename):
 
 
 @app.post("/files/<path:filename>")
-async def modify_file(filename):
+async def edit_file(filename):
     request_data = await quart.request.get_json(force=True)
     filename = unquote(filename)
     fixes = request_data.get("fixes", [])
+    method = request_data.get("method", "subs")
+
+    if method == "replace" and len(fixes) != 1:
+        return quart.Response(response='Error: For "replace" method, only one fix should be provided', status=400)
 
     with open(filename, 'r') as f:
         lines = f.readlines()
@@ -125,10 +240,21 @@ async def modify_file(filename):
         new_code = fix["code"]
         indentation = fix["indentation"]
         indented_code = '\n'.join([indentation + line for line in new_code.split('\n')])
-        lines[start_line - 1:end_line] = [indented_code + '\n']
+
+        if method == "subs":
+            lines[start_line - 1:end_line] = [indented_code + '\n']
+        elif method == "insert":
+            lines.insert(start_line - 1, indented_code + '\n')
+        elif method == "replace":
+            lines = [indented_code + '\n']
 
     with open(filename, 'w') as f:
         f.writelines(lines)
+
+    # If the file is a Python file, refactor it
+    if filename.endswith('.py'):
+        # Assuming you have installed the 'black' Python code formatter
+        subprocess.run(['black', filename], check=True)
 
     return quart.Response(response='OK', status=200)
 
@@ -170,6 +296,11 @@ async def options_files(filename):
     response.headers['Access-Control-Allow-Headers'] = 'content-type,openai-conversation-id,openai-ephemeral-user-id'
     response.headers['Access-Control-Allow-Methods'] = 'GET,POST'
     return response
+
+
+def remove_ansi_escape_sequences(text):
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    return ansi_escape.sub('', text)
 
 
 def main():
---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+cd D:\Pandora\debuggerGPT
+call conda activate debuggerGPT
+python main.py
+pause
